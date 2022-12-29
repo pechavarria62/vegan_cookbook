6cf303695267fea98b0ed5ed763986a8cdebf4bc
Pedro Echavarria <pechavarria62@gmail.com>
2022-12-28 23:08:11 -0500

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
-function App() {
+const App =()=> {
   return (
     <div className="App">
-      <nav>
         <Header/>
-      </nav>
-      <body>
+        
         <Body/>
-      </body>
-      <footer>
+
+
         <Footer/>
-      </footer>
+
     </div>
   );
-}
+};
 
 export default App;
