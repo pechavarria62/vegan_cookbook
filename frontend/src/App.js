c66f0777e4896541a630684ba258d436c1a9ed8d
Pedro Echavarria <pechavarria62@gmail.com>
2023-01-20 22:38:18 -0500

    Deleted React-BootStrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Favorites from './pages/Favorites';
 import News from './pages/News';
+import Registration from './pages/Registration';
 
 import Header from './components/Header';
 import Body from './components/Body';
@@ -20,6 +20,7 @@ const App =()=> {
           <Route path="/Explore" element={<Explore />} />
           <Route path="/Favorites" element={<Favorites />} />
           <Route path="/News" element={<News />} />
+          <Route path="/Registration" element={<Registration />} />
         </Routes>
         <Body/>
       </div>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,75 +1,9 @@
 import React from 'react';
-import './static/Header.css';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import vegan from '../images/vegan.png';
+// import vegan from '../images/vegan.png';
 
 export default function Header() {
     
     return (
-        <Navbar bg="light" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="#">
-                    <img
-                        alt="A plant growing in half of the world map"
-                        src={vegan}
-                        style={{
-                            maxWidth: "84px",
-                            height: "64px",
-                            padding: "8px",
-                            marginRight: "8px",
-                        }}
-                    />
-                    VeganBook
-                </Navbar.Brand >
-                    <Form className="d-flex">
-                        <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                        />
-                    </Form>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse 
-                    id="navbarScroll"
-                    className='right-aligned'
-                    
-                >
-                    <Nav
-                        className=" my-2 my-lg-0 "
-                        style={{ 
-                            maxHeight: '100px',
-                        }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="/home">Home</Nav.Link>
-                        <Nav.Link href="/Explore">Explore</Nav.Link>
-                        <Nav.Link href="/Favorites">Favorites</Nav.Link>
-                        <Nav.Link href="/News">News</Nav.Link>
-                        <NavDropdown title="Me!" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">
-                                Comming
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        {/* <NavDropdown title="Account" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">login</NavDropdown.Item>
-                        </NavDropdown> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div>klk</div>
     );
 };
-
-// export default HeaderContent;
